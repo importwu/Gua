230faa65ae5ab4f6a9b68695cb9695d5c467cb84
wuzihao <wuzihao@localhost.localdomain>
2019-04-07 18:52:00 +0800

    add .gitignore

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,17 +3,18 @@ plugins {
 }
 
 group = "wuz.gua"
-version = "1.0-SNAPSHOT"
+version = "1.0"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    compile("org.antlr", "antlr4-runtime", "4.0")
-    compile("org.scala-lang", "jline", "2.10.7")
+    implementation("org.antlr", "antlr4-runtime", "4.0")
+    implementation("org.scala-lang", "jline", "2.10.7")
 }
 
 configure<JavaPluginConvention> {
     sourceCompatibility = JavaVersion.VERSION_1_8
 }
+
